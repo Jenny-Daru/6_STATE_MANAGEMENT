44c753a7a146c408709ab4e2683ba487e461b956
jenny <dooly3392@gmail.com>
2024-02-22 00:03:47 +0900

    useRecoilState: state 값, setter 함수 모두 리턴 useRecoilValue: state 값 리턴 useSetRecoilState: setter 함수 리턴

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,13 +1,29 @@
 import { useForm } from "react-hook-form";
+import { atom, useRecoilState } from "recoil";
 
 interface IForm {
   toDo: string;
 }
 
+interface IToDo {
+  text: string;
+  id: number;
+  category: "TO_DO" | "DOING" | "DONE";
+}
+
+const toDoState = atom<IToDo[]>({
+  key: "toDo",
+  default: [],
+});
+
 export default function TodoList() {
+  const [toDos, setToDos] = useRecoilState(toDoState);
   const { register, handleSubmit, setValue } = useForm<IForm>();
-  const handleValid = (data: IForm) => {
-    console.log("add to do", data.toDo);
+  const handleValid = ({ toDo }: IForm) => {
+    setToDos((oldToDos) => [
+      { text: toDo, id: Date.now(), category: "TO_DO" },
+      ...oldToDos,
+    ]);
     setValue("toDo", "");
   };
   return (
@@ -16,6 +32,11 @@ export default function TodoList() {
         <input {...register("toDo")} placeholder="Write a to do"></input>
         <button>Add</button>
       </form>
+      <ul>
+        {toDos.map((toDo) => (
+          <li key={toDo.id}>{toDo.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
